ceb3c888df3cca2688165afee33b82bc4f20a6f1
유하람 <dlfandl97@naver.com>
2025-01-15 17:11:49 +0900

    vue 프로젝트 생성

---
+++ oauth2-back/src/main/java/com/ssafy/oauth2back/Oauth2BackApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.oauth2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2BackApplication.class, args);
+    }
+
+}
---
+++ oauth2-back/src/test/java/com/ssafy/oauth2back/Oauth2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.oauth2back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
